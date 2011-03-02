8e506e7c9f1f0fe4d50a2a08cb8fa9d1f5ef0f47
huy <huy@huyng.com>
2011-03-02 08:33:51 -0800

    remove piping to less

--- bashmarks.sh
+++ bashmarks.sh
@@ -92,7 +92,7 @@ function l {
     source $SDIRS
     
     # if color output is not working for you, comment out the line below '\033[1;32m' == "red"
-    env | sort | awk '(substr($0,0,4)=="DIR_"){split(substr($0,5),parts,"="); printf("\033[1;31m%-20s\033[0m %s\n", parts[1], parts[2]);}' | less -R
+    env | sort | awk '(substr($0,0,4)=="DIR_"){split(substr($0,5),parts,"="); printf("\033[1;31m%-20s\033[0m %s\n", parts[1], parts[2]);}'
     
     # uncomment this line if color output is not working with the line above
     # env | grep "^DIR_" | cut -c5- | sort |grep "^.*=" 
